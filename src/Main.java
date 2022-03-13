20da2736352f8f3a30b3eb04076b14c409f679a6
icelemon-czy <71572625+icelemon-czy@users.noreply.github.com>
2022-03-13 13:52:19 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+import java.io.File;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    /**
+     * Input : input file
+     */
+    public static void Goint(File )
+}
