64101d6a48c3cc99d4f0e8dfe411e414acc1baec
icelemon-czy <71572625+icelemon-czy@users.noreply.github.com>
2022-03-31 00:09:16 -0400

    /Update/

--- src/BranchTargetBuffer.java
+++ src/BranchTargetBuffer.java
@@ -1,2 +1,20 @@
+/**
+ * Responsibility :
+ * 1. Store Target and Address ( pair of target and address)
+ * 2. Add (target,address)
+ * 3. Search (target,address)
+ */
+import java.util.HashMap;
 public class BranchTargetBuffer {
+    HashMap<String,Integer> branchTarget;
+    public BranchTargetBuffer(){
+        branchTarget = new HashMap<>();
+    }
+    public void add(String target,int address){
+        branchTarget.put(target,address);
+    }
+    public int getAddress(String target){
+        return branchTarget.get(target);
+    }
+
 }
